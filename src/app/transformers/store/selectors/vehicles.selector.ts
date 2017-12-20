572a7e689f5ac9086340a16ab18b16dffc40c439
anmarusic <toni_marusic@hotmail.com>
2017-12-20 10:03:34 +0100

    third commit

--- src/app/transformers/store/selectors/vehicles.selector.ts
+++ src/app/transformers/store/selectors/vehicles.selector.ts
@@ -1,6 +1,6 @@
 import { createSelector } from '@ngrx/store'
 
-import * as fromRoot from '../../../app/store'
+import * as fromRoot from '../../../store'
 import * as fromFeature from '../reducers'
 import * as fromVehicles from '../reducers/vehicles.reducer'
 
